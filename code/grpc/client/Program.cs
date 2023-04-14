b98a96247e7d713e37b2a79a569e5e07684204d8
briandenicola <brian@briandenicola.com>
2023-04-14 14:11:54 -0500

    dotnet grpc client

---
+++ code/grpc/client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+using Grpc.Net.Client;
+using Grpc.Core;
+using Client;
+
+var builder = new ConfigurationBuilder().AddCommandLine(args);
+var config = builder.Build();
+
+var address = config["GrpcEndpoint"];
+
+Console.WriteLine($"Connecting to {address}");
+
+var channel = GrpcChannel.ForAddress(address);
+var client = new Greeter.GreeterClient(channel);
+
+var replies = client.SayHelloStream(
+    new HelloRequest(){ Name = "Brian" },
+    deadline: DateTime.UtcNow.AddMinutes(10)
+);
+
+try 
+{
+    var reply = client.SayHello(
+        new HelloRequest(){ Name = "Brian" }
+    );
+    Console.WriteLine($"Reply: {reply.Message}");
+    
+    await foreach (var streamreply in replies.ResponseStream.ReadAllAsync())
+    {
+        Console.WriteLine($"Reply: {streamreply.Message}");
+    }
+}
+catch (RpcException ex) when (ex.StatusCode == StatusCode.DeadlineExceeded)
+{
+    Console.WriteLine("Stream timed out after 10 minutes.");
+}
